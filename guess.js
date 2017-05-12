9b1268277425845c0029509c58be37499bfd2e62
Guilherme Ferreira <360guilherme@gmail.com>
2017-05-12 18:35:48 -0300

    pegadinha adicionada

--- guess.js
+++ guess.js
@@ -124,7 +124,8 @@ window.onload = function () {
             /*["everton", "liverpool", "swansea", "chelsea", "hull", "manchester-city", "newcastle-united"],
             ["alien", "dirty-harry", "gladiator", "finding-nemo", "jaws"],
             ["manchester", "milan", "madrid", "amsterdam", "prague"]*/
-            ["kazoo"]
+            ["kazoo"],
+            ["guilherme"]
         ];
 
         chosenCategory = categories[Math.floor(Math.random() * categories.length)];
@@ -151,7 +152,8 @@ window.onload = function () {
            /* ["Based in Mersyside", "Based in Mersyside", "First Welsh team to reach the Premier Leauge", "Owned by A russian Billionaire", "Once managed by Phil Brown", "2013 FA Cup runners up", "Gazza's first club"],
             ["Science-Fiction horror film", "1971 American action film", "Historical drama", "Anamated Fish", "Giant great white shark"],
             ["Northern city in the UK", "Home of AC and Inter", "Spanish capital", "Netherlands capital", "Czech Republic capital"]*/
-           ["Pode ser feito de metal ou de plástico", "É um instrumento músical", "Foi inventado na Africa"]
+           ["Pode ser feito de metal ou de plástico", "É um instrumento músical", "Foi inventado na Africa"],
+            ["É o seu irmão preferido", "Merece um ótimo presente no proximo dia 14", "Uma pessoa muito legal"]
         ];
 
         var catagoryIndex = categories.indexOf(chosenCategory);
